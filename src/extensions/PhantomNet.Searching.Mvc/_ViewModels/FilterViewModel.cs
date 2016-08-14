b0c44875f2faed8c36d5485adae85838658ceed3
Minh Nguyen <mnguyen284@gmail.com>
2016-08-15 01:55:13 +0700

    FilterViewModel

---
+++ src/extensions/PhantomNet.Searching.Mvc/_ViewModels/FilterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace PhantomNet.Searching.Mvc
+{
+    public interface IFilterViewModel<TOptionViewModel>
+        where TOptionViewModel : FilterOptionViewModel
+    {
+        IEnumerable<string> ParameterNames { get; set; }
+
+        string DisplayText { get; set; }
+
+        IEnumerable<TOptionViewModel> Options { get; set; }
+
+        IEnumerable<TOptionViewModel> ActiveOptions { get; set; }
+    }
+
+    public class FilterViewModel<TOptionViewModel> : IFilterViewModel<TOptionViewModel>
+        where TOptionViewModel : FilterOptionViewModel
+    {
+        public IEnumerable<string> ParameterNames { get; set; }
+
+        public string DisplayText { get; set; }
+
+        public IEnumerable<TOptionViewModel> Options { get; set; }
+
+        public IEnumerable<TOptionViewModel> ActiveOptions { get; set; }
+    }
+}
